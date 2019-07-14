b6eb521b17f124e230a0915423f7c28af3669980
pancreasio <julianholisu@gmail.com>
2019-07-14 14:46:35 -0300

    finished player movement
    imported player sprite

---
+++ Moon-Crasher/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float rotationSpeed, movementSpeed, boostSpeed;
+    private Rigidbody2D rig;
+    private bool boost;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        boost = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rig.AddForce(transform.up * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space) && boost)
+        {
+            rig.AddForce(transform.up * boostSpeed, ForceMode2D.Impulse);
+            boost = false;
+        }
+    }
+}
