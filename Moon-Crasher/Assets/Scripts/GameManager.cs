c5b8a87fcec38031b4d7a91f1f295560fa7a286f
pancreasio <julianholisu@gmail.com>
2019-07-14 13:49:15 -0300

    Splash screen Finished

---
+++ Moon-Crasher/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    internal void Menu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    internal void Game()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    internal void GameOver()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    internal void Splash()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Moon-Crasher/Assets/Scripts/SplashManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashManager : MonoBehaviour
+{
+    GameManager gameManager;
+    public Image company, title;
+    private bool screenSwitch;
+    float remainingTime, colorMultiplier = 5;
+
+    private void Start()
+    {
+        screenSwitch = true;
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        remainingTime = 1.0f;
+        StartCoroutine(FadeScreen(company));
+    }
+
+    private void Update()
+    {
+        remainingTime -= Time.deltaTime;
+        if (remainingTime <= 0 && screenSwitch)
+        {
+            company.gameObject.SetActive(false);
+            remainingTime = 1.0f;
+            StartCoroutine(FadeScreen(title));
+            screenSwitch = false;
+        }
+
+        if (remainingTime <= 0 && !screenSwitch)
+        {
+            title.gameObject.SetActive(false);
+            LoadMenu();
+        }
+    }
+
+    IEnumerator FadeScreen(Image screen)
+    {
+        screen.gameObject.SetActive(true);
+        Color fadeAmmount = screen.material.color;
+        remainingTime -= Time.deltaTime;
+        while (remainingTime >= 0)
+        {
+            fadeAmmount.a = remainingTime * colorMultiplier;
+            screen.material.color = fadeAmmount;
+            yield return null;
+        }
+    }
+
+    void LoadMenu()
+    {
+        gameManager.Menu();
+    }
+}
