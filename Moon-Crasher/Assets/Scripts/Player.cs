64be8afc55c8933c5ba40c0b0a72e15e904c0fae
pancreasio <julianholisu@gmail.com>
2019-07-14 16:41:09 -0300

    player bounds implemented

--- Moon-Crasher/Assets/Scripts/Player.cs
+++ Moon-Crasher/Assets/Scripts/Player.cs
@@ -6,10 +6,14 @@ public class Player : MonoBehaviour
 {
     public float rotationSpeed, movementSpeed, boostSpeed;
     private Rigidbody2D rig;
+    private Vector2 maxX, minX, maxY;
     private bool boost;
 
     private void Start()
     {
+        maxX = GameObject.Find("MaxX").transform.position;
+        minX = GameObject.Find("MinX").transform.position;
+        maxY = GameObject.Find("Player MaxY").transform.position;
         rig = GetComponent<Rigidbody2D>();
         boost = true;
     }
@@ -33,5 +37,24 @@ public class Player : MonoBehaviour
             rig.AddForce(transform.up * boostSpeed, ForceMode2D.Impulse);
             boost = false;
         }
+
+        if (transform.position.x < minX.x)
+        {
+            transform.position = new Vector2(maxX.x, transform.position.y);
+        }
+        if (transform.position.x > maxX.x)
+        {
+            transform.position = new Vector2(minX.x, transform.position.y);
+        }
+
+        if (transform.position.y > maxY.y)
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Destroy(this.gameObject);
     }
 }
